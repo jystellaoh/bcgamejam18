c349c00473edf82daf2173e87dda3d30243bc3bc
Michael Goh <mikegoh1997@gmail.com>
2018-02-10 14:28:49 -0800

    Added script to work with selection button

---
+++ Dream not Scream/Assets/Scripts/MovePiece.cs
@@ -0,0 +1,10 @@
+﻿using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class MovePiece : MonoBehaviour, IInputClickHandler {
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        this.gameObject.SetActive(false); 
+    }
+}
