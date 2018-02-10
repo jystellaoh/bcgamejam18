d9f4d2a9547d344ce76f01450d5e3ed024039513
Yana Pertels <ypertels@gmail.com>
2018-02-10 14:38:00 -0800

    whackaneyemole

---
+++ Dream not Scream/Assets/Scripts/EyemoleManager.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Linq;
+
+namespace HoloToolkit.Unity.InputModule.Examples.Grabbables
+{
+    /// <summary>
+    /// Simple class to change the color of grabbable objects based on state
+    /// </summary>
+    public class EyemoleManager : MonoBehaviour
+    {
+
+        [SerializeField]
+        public AudioSource[] phrases;
+        [SerializeField]
+        public GameObject mole;
+
+        public float spawnRadius;
+
+
+        private float timeSinceLastSpawn = 0f;
+        private float timeBetweenSpawns = 0.5f;
+        
+        private void Awake()
+        {
+
+        }
+
+        private void Update() {
+
+            if (Time.time - timeSinceLastSpawn >= timeBetweenSpawns) {
+                SpawnEyemole();
+            }
+
+        }
+
+        private void SpawnEyemole() {
+
+            GameObject eyemoleObj = Instantiate(mole);
+            mole.transform.parent = gameObject.transform;
+            System.Random rnd = new System.Random();
+            mole.transform.localPosition += new Vector3(Mathf.Sin(rnd.Next(0, 360) * 2 * Mathf.PI / 180), 0, Mathf.Cos(rnd.Next(0, 360) * 2 * Mathf.PI / 180));
+            System.Random rndP = new System.Random();
+            AudioSource randomPhrase = phrases[(int) rndP.Next(0, phrases.Count())];
+            mole.GetComponent<WhackEyemole>().SetPhrase(randomPhrase);
+        }
+    }
+}
---
+++ Dream not Scream/Assets/Scripts/WhackEyemole.cs
@@ -0,0 +1,95 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace HoloToolkit.Unity.InputModule.Examples.Grabbables
+{
+    /// <summary>
+    /// Simple class to change the color of grabbable objects based on state
+    /// </summary>
+    public class WhackEyemole : MonoBehaviour
+    {
+
+        [SerializeField]
+        private BaseGrabbable grabbable;
+        [SerializeField]
+        public AudioSource deathSound;
+        [SerializeField]
+        private AudioSource phrase = null;
+
+        private float minY = -5f; 
+        private float baseY = 0.1f;
+
+        private bool isDying = false;
+
+        private float timeSinceLastPhrase = 0f;
+        private float timeBetweenPhrases = 0.5f;
+        
+        private void Awake()
+        {
+            if (grabbable == null)
+            {
+                grabbable = GetComponent<BaseGrabbable>();
+            }
+          
+            grabbable.OnContactStateChange += Whack;
+        }
+
+        public void SetPhrase(AudioSource phr) {
+            this.phrase = phr;
+        }
+
+        private void Update() {
+
+            Vector3 molePosition = gameObject.transform.position;
+
+            if (isDying) {
+                molePosition.y -= 0.01f;
+                gameObject.transform.position = molePosition;
+            } 
+            else if (Time.time - timeSinceLastPhrase >= timeBetweenPhrases) {
+                phrase.Play();
+                timeSinceLastPhrase = Time.time;
+            }
+
+            if (gameObject.transform.position.y <= baseY) {
+                molePosition += Vector3.up * 0.01f;
+                gameObject.transform.position = molePosition;
+            }
+
+            if (gameObject.transform.position.y <= minY) {
+                Destroy(gameObject);
+            }
+        }
+
+        private void Whack(BaseGrabbable baseGrab)
+        {
+
+            Debug.Log(baseGrab.ContactState);
+
+            switch (baseGrab.ContactState)
+            {
+                case GrabStateEnum.Inactive:
+                    break;
+
+                case GrabStateEnum.Multi:
+                    isDying = true;
+                    deathSound.Play();
+                    break;
+
+                case GrabStateEnum.Single:
+                    isDying = true;
+                    deathSound.Play();
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+
+        }
+    }
+}
