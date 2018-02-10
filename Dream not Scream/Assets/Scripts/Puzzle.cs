46dd01b292b8c3a023e4638d9dd6a7c40daa9450
Michael Goh <mikegoh1997@gmail.com>
2018-02-10 11:17:31 -0800

    Added initial input example

---
+++ Dream not Scream/Assets/Scripts/Puzzle.cs
@@ -0,0 +1,11 @@
+﻿using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class Puzzle : MonoBehaviour, IInputClickHandler {
+
+    
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        Debug.Log("Hello");
+    }
+}
