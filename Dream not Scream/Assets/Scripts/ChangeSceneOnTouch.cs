24a79ce1877b0925791f9992c0fb34a13720f3b9
Yana Pertels <ypertels@gmail.com>
2018-02-10 11:00:51 -0800

    scene chnge on touch

---
+++ Dream not Scream/Assets/Scripts/ChangeSceneOnTouch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneOnTouch : MonoBehaviour {
+
+	public int sceneToChangeTo = 1;
+
+	// Use this for initialization
+	void Start () {
+		//interactableObject = GetComponentInParent<VRTK_InteractableObject> ();
+		//interactableObject.InteractableObjectGrabbed += new InteractableObjectEventHandler (ChangeScene);
+	}
+
+	// Update is called once per frame
+	void OnTiggerEnter (Collider other) {
+
+		if (other.gameObject.tag == "GameController") {
+			ChangeScene ();
+		}
+
+	}
+
+	void ChangeScene(){
+		SceneManager.LoadScene (sceneToChangeTo);
+	}
+}
